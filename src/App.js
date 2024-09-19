f3fabdbe35406408b3f80890aa7c96a4d1b89466
Guzman <francisco.guzman@accenture.com>
2024-09-18 22:59:42 -0400

    react router

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Currencies from "./pages/currencies";
+import Main from "./pages/main";
+import Price from "./pages/price";
+import Nav from "./components/nav";
+
+export default function App () {
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main/>}/>
+        <Route path="/currencies" element={<Currencies/>}/>
+        <Route path="/price" element={<Price/>}/>
+      </Routes>
+    </div>
+  );
+}
